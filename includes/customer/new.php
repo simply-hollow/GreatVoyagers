670632236a012726d40f16b9ca0fc7d077bf41d6
Harshgupta <harshguptashnd@gmail.com>
2019-10-10 23:09:37 +0530

    changes in existing.php

--- includes/customer/new.php
+++ includes/customer/new.php
@@ -1,6 +1,19 @@
+
 <div class="col-12">
 <div class="card">
 <div class="card-body">
+<form class="form-sample">
+    <div class="row">
+      <div class="col-md-10 ">
+        <div class="custom-file mb-3">
+          <input type="file" class="custom-file-input form-control" id="customFile" name="filename">
+          <label class="custom-file-label" for="customFile">Choose file</label>
+        </div>
+      </div>
+    
+    </div>
+  </form>
+  <br>
   <form class="form-sample">
     <div class="row">
       <div class="col-md-6">
@@ -23,20 +36,35 @@
     <div class="row">
       <div class="col-md-6">
         <div class="form-group row">
-          <label class="col-sm-3 col-form-label">Gender</label>
+          <label class="col-sm-3 col-form-label">Father's Name</label>
           <div class="col-sm-9">
-            <select class="form-control">
-              <option>Male</option>
-              <option>Female</option>
-            </select>
+            <input type="text" class="form-control" />
           </div>
         </div>
       </div>
       <div class="col-md-6">
         <div class="form-group row">
-          <label class="col-sm-3 col-form-label">Date of Birth</label>
+          <label class="col-sm-3 col-form-label">Mother's Name</label>
           <div class="col-sm-9">
-            <input class="form-control" placeholder="dd/mm/yyyy" />
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Phone No.</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Email Id</label>
+          <div class="col-sm-9">
+            <input type="Email" class="form-control" />
           </div>
         </div>
       </div>
@@ -47,43 +75,91 @@
           <label class="col-sm-3 col-form-label">Category</label>
           <div class="col-sm-9">
             <select class="form-control">
-              <option>Category1</option>
-              <option>Category2</option>
-              <option>Category3</option>
-              <option>Category4</option>
+              <option>General</option>
+              <option>OBC</option>
+              <option>SC/ST</option>
             </select>
           </div>
         </div>
       </div>
       <div class="col-md-6">
         <div class="form-group row">
-          <label class="col-sm-3 col-form-label">Membership</label>
-          <div class="col-sm-4">
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios1" value="" checked> Free </label>
-            </div>
-          </div>
-          <div class="col-sm-5">
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios2" value="option2"> Professional </label>
-            </div>
+          <label class="col-sm-3 col-form-label">Date of Birth</label>
+          <div class="col-sm-9">
+            <input  type="date" class="form-control"/>
           </div>
         </div>
       </div>
     </div>
-    <p class="card-description"> Address </p>
+    <div class="row">
+    <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Gender</label>
+          <div class="col-sm-3">
+            <div class="form-check">
+              <label class="form-check-label">
+                <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios1" value="" checked> Male </label>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="form-check">
+              <label class="form-check-label">
+                <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios1" value="" > Others</label>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="form-check">
+              <label class="form-check-label">
+                <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios2" value="option2"> Female </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Aadhar No.</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="row">
       <div class="col-md-6">
         <div class="form-group row">
-          <label class="col-sm-3 col-form-label">Address 1</label>
+          <label class="col-sm-3 col-form-label">PanCard No.</label>
           <div class="col-sm-9">
             <input type="text" class="form-control" />
           </div>
         </div>
       </div>
       <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Passport No.</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="card-description"> Address </p>
+    
+    <div class="row">
+    <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Country</label>
+          <div class="col-sm-9">
+            <select class="form-control">
+            <option>India</option>
+              <option>America</option>
+              <option>Italy</option>
+              <option>Russia</option>
+              <option>Britain</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    <div class="col-md-6">
         <div class="form-group row">
           <label class="col-sm-3 col-form-label">State</label>
           <div class="col-sm-9">
@@ -93,9 +169,9 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-6">
+    <div class="col-md-6">
         <div class="form-group row">
-          <label class="col-sm-3 col-form-label">Address 2</label>
+          <label class="col-sm-3 col-form-label">City</label>
           <div class="col-sm-9">
             <input type="text" class="form-control" />
           </div>
@@ -113,7 +189,7 @@
     <div class="row">
       <div class="col-md-6">
         <div class="form-group row">
-          <label class="col-sm-3 col-form-label">City</label>
+          <label class="col-sm-3 col-form-label">Address 1</label>
           <div class="col-sm-9">
             <input type="text" class="form-control" />
           </div>
@@ -121,14 +197,9 @@
       </div>
       <div class="col-md-6">
         <div class="form-group row">
-          <label class="col-sm-3 col-form-label">Country</label>
+          <label class="col-sm-3 col-form-label">Address 2</label>
           <div class="col-sm-9">
-            <select class="form-control">
-              <option>America</option>
-              <option>Italy</option>
-              <option>Russia</option>
-              <option>Britain</option>
-            </select>
+            <input type="text" class="form-control" />
           </div>
         </div>
       </div>
@@ -138,6 +209,6 @@
       <button type="reset" class="btn btn-gradient-dark btn-lg mr-2" >Reset</button>
     </center>
   </form>
-</div>
-</div>
-</div>  
+  </div>
+  </div>
+  </div>
