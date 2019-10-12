bea64eff0d21d7697b5bfd6f36fd277b0f0c4149
Harshgupta <harshguptashnd@gmail.com>
2019-10-12 22:39:25 +0530

    NO linking is done

---
+++ includes/taskmgmt/assign.php
@@ -0,0 +1,50 @@
+<div class="col-12">
+<div class="card">
+<div class="card-body">
+  <form class="form-sample">
+    <div class="row">
+    <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Employee ID</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" placeholder="Enter Employee Id" required/>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Assigned Task</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" placeholder="Enter Task assigned"  required/>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div class="row">
+    <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Customer Id</label>
+          <div class="col-sm-9">
+            <input type="text" class="form-control" placeholder="Customer Id" required/>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="form-group row">
+          <label class="col-sm-3 col-form-label">Deadline</label>
+          <div class="col-sm-9">
+            <input type="Date" class="form-control"  required/>
+          </div>
+        </div>
+      </div>
+    </div>
+
+       <center>
+      <button type="submit" class="btn btn-gradient-success btn-lg mr-2">Submit</button>
+      <button type="reset" class="btn btn-gradient-dark btn-lg mr-2" >Reset</button>
+    </center>
+    
+  </form>
+</div>
+</div>
+</div>  
